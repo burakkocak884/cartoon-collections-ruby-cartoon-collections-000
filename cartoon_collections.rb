2922931d75edb85ffed54cfe7ab491e4e545bfac
BURAK KOCAK <burakkocak884@hotmail.com>
2018-12-02 04:48:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
 
+
+def roll_call_dwarves(dwarves)
+
+dwarves.each_with_index do  |dwarf, index|
+
+puts "/#{index}.*#{dwarf}/ \n"
+
+end
+end
 def summon_captain_planet# code an argument here
   # Your code here
 end
